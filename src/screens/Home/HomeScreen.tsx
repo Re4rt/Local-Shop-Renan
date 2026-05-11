73702a1953b0e511a9458477cb44da60fa7c847d
Re4rt <renanhiroshioshima@gmail.com>
2026-05-11 16:25:28 -0300

    outros bugs

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,18 +1,36 @@
-import { View, FlatList, ActivityIndicator } from 'react-native';
+import React from 'react';
+import { View, FlatList, ActivityIndicator, TouchableOpacity, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; 
 import { useLojas } from '../../hooks/useLojas';
 import LojaCard from '../../components/LojaCard';
 
+
+import { styles } from './StyleHome'; 
+
 export default function HomeScreen() {
   const { lojas, loading } = useLojas();
+  const navigation = useNavigation();
 
-  if (loading) return <ActivityIndicator size="large" color="#27ae60" />;
+  if (loading) return <ActivityIndicator size="large" color="#27ae60" style={{ flex: 1 }} />;
 
   return (
-    <View style={{ flex: 1 }}>
-      <FlatList 
-        data={lojas}
-        renderItem={({ item }) => <LojaCard loja={item} />}
-      />
-    </View>
-  );
+  <View style={styles.container}>
+    
+    <FlatList 
+      data={lojas}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => <LojaCard loja={item} />}
+      contentContainerStyle={{ paddingBottom: 100 }} 
+    />
+
+    
+    <TouchableOpacity 
+      style={styles.buttonCadastro} 
+      onPress={() => navigation.navigate('Cadastro' as never)}
+    >
+      <Text style={styles.buttonText}>+</Text>
+    </TouchableOpacity>
+
+  </View>
+);
 }
